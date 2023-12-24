456eefeb0d6b42128a3ae97c42623c4199168f74
AbhishekBhavnani2003 <147069739+AbhishekBhavnani2003@users.noreply.github.com>
2023-12-24 11:43:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+
+function togggleNavbar() 
+{ 
+    var navbar = document.querySelector('.navbar') ; 
+    navbar.style.display=
+    (navbar.style.display === 'none' || navbar.style.display === ' ') ? 'block' : 'none' ; 
+} 
+
+
+
+
+
+
+
+const previous = document.querySelector('.previous') ;
+const next = document.querySelector('.next') ;
+const images = document.querySelector('.sliderphotos').children; 
+
+const length = images.length
+
+let current = 0 ; 
+
+previous.addEventListener('click' , () =>
+{
+    previousimg() ; 
+})
+next.addEventListener('click' , () =>
+{
+    nextimg() ; 
+})
+
+
+setInterval (() =>
+{
+    nextimg() ; 
+},2000) ;
+
+
+
+function previousimg()
+{
+    images[current].classList.remove('main');
+    if ( current== 0)
+    {
+        current=length-1 ; 
+    }
+
+    else{
+        current-- ; 
+    }
+    images[current].classList.add('main') ;
+}
+
+
+function nextimg()
+{
+    images[current].classList.remove('main');
+    if ( current==length-1 )
+    {
+        current=0; 
+    }
+
+    else{
+        current++; 
+    }
+
+    images[current].classList.add('main') ;
+}
