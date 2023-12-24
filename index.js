738b776bc5a8152a1abce84980c8625e29ba37f8
AbhishekBhavnani2003 <147069739+AbhishekBhavnani2003@users.noreply.github.com>
2023-12-24 13:56:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function togggleNavbar()
     var navbar = document.querySelector('.navbar') ; 
     navbar.style.display=
     (navbar.style.display === 'none' || navbar.style.display === ' ') ? 'block' : 'none' ; 
+    var navigationbutton = document.querySelector('.navigationbutton');
+    navigationbutton.style.display='none' ;
 } 
 
 
